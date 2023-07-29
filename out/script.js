08de33a1737eabc112a5ac8ad08cb8e9fc01d02d
Mark Kennedy <34138255+mmmrkennedy@users.noreply.github.com>
2023-07-29 16:10:58 -0400

    die_machine and styles and JS implement

---
+++ out/script.js
@@ -0,0 +1,12 @@
+function toggleDarkMode() {
+    const body = document.body;
+    body.classList.toggle('dark-mode');
+}
+
+function navigateToIndex() {
+    window.location.href = "../../index.html";
+}
+
+function scrollToTop() {
+    window.scrollTo(0, 0);
+}
