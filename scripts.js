9a5b75a9a74140ff9fd53df5c55bf7836eaaaea6
Mark Kennedy <markkennedy132@gmail.com>
2023-09-17 23:58:25 -0400

    Finished Most of IX
    Finished side quests with pictures, main quest pictures and iron bull pictures are only thing left

--- scripts.js
+++ scripts.js
@@ -4,21 +4,6 @@ function toggleLightMode() {
     body.classList.toggle('light-mode');
 }
 
-// Function to scroll smoothly to a specific HTML element by its ID with an offset
-function scrollToElement(elementId, offset) {
-    const element = document.getElementById(elementId);
-
-    if (element) {
-        const elementPosition = element.getBoundingClientRect().top;
-        const targetY = elementPosition + window.scrollY - offset;
-
-        window.scrollTo({
-            top: targetY,
-            behavior: 'smooth'
-        });
-    }
-}
-
 // Function to navigate to a specific URL
 function navigateToIndex() {
     window.location.href = "/zombiesGuides/index.html";
@@ -37,8 +22,8 @@ const fontSelector = document.getElementById('fontSelector');
 const smoothScroll = document.querySelector('.smooth-scroll');
 
 // Event listener for font selector changes
-var path = window.location.pathname;
-var page = path.split("/").pop();
+let path = window.location.pathname;
+let page = path.split("/").pop();
 
 if(page !== "index.html") {
     fontSelector.addEventListener('change', () => {
@@ -77,25 +62,38 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+// Function to scroll smoothly to a specific HTML element by its ID with an offset
+function scrollToElement(elementId, offset) {
+    const element = document.getElementById(elementId);
 
+    if (element) {
+        const elementPosition = element.getBoundingClientRect().top;
+        const targetY = elementPosition + window.scrollY - offset;
+
+        window.scrollTo({
+            top: targetY,
+            behavior: 'smooth'
+        });
+    }
+}
 
 /* Mobile Detection */
 function hasTouchScreen() {
-    var hasTouchScreen = false;
+    let hasTouchScreen;
 
     if ("maxTouchPoints" in navigator) {
         hasTouchScreen = navigator.maxTouchPoints > 0;
     } else if ("msMaxTouchPoints" in navigator) {
         hasTouchScreen = navigator.msMaxTouchPoints > 0;
     } else {
-        var mQ = window.matchMedia && matchMedia("(pointer:coarse)");
+        let mQ = window.matchMedia && matchMedia("(pointer:coarse)");
         if (mQ && mQ.media === "(pointer:coarse)") {
             hasTouchScreen = !!mQ.matches;
         } else if ('orientation' in window) {
             hasTouchScreen = true; // deprecated, but good fallback
         } else {
             // Only as a last resort, fall back to user agent sniffing
-            var UA = navigator.userAgent;
+            let UA = navigator.userAgent;
             hasTouchScreen = (
                 /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
                 /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA)
@@ -106,7 +104,7 @@ function hasTouchScreen() {
 }
 
 function mobileCheck() {
-    var check = hasTouchScreen();
+    let check = hasTouchScreen();
 
     if (check) {
         // Show the mobile content and hide the non-mobile content
@@ -123,83 +121,21 @@ document.addEventListener("DOMContentLoaded", function() {
     mobileCheck();
 });
 
-/* Hover Image
-document.addEventListener("DOMContentLoaded", function() {
-
-    let imageHoverEnabled = localStorage.getItem('image-hover');
-    if (imageHoverEnabled === 'false') {  // Again, check against the string 'false'
-        return;  // Exit if image hover is disabled
+document.addEventListener('click', function(event) {
+    var target = event.target;
+    if (target.tagName.toLowerCase() === 'a' && target.hash) {
+        window.history.pushState({hash: target.hash}, '', target.hash);
     }
-
-    const links = document.querySelectorAll(".hover-image");
-    const popup = document.createElement("img");
-    popup.id = "popup-image";
-    popup.style.position = "fixed";
-    document.body.appendChild(popup);
-
-    let hideTimeout;
-
-    let currentLink = null;  // To keep track of the currently hovered link
-
-    links.forEach(function(link) {
-        link.addEventListener("mouseover", function() {
-            currentLink = this;  // Set the currently hovered link
-            clearTimeout(hideTimeout);  // Clear any existing timeout to hide the image
-            popup.src = this.href;
-
-            const IMG_WIDTH_PERCENT = 0.95;  // For 95% of screen width
-            const IMG_HEIGHT_PERCENT = 1; // For 100% of screen height
-
-            popup.onload = function() {
-                const screenWidth = window.innerWidth;
-                const screenHeight = window.innerHeight;
-
-                const aspectRatio = popup.naturalWidth / popup.naturalHeight;
-                let imgWidth = screenWidth * IMG_WIDTH_PERCENT;
-                let imgHeight = imgWidth / aspectRatio;
-
-                if (imgHeight > screenHeight * IMG_HEIGHT_PERCENT) {
-                    imgHeight = screenHeight * IMG_HEIGHT_PERCENT;
-                    imgWidth = imgHeight * aspectRatio;
-                }
-
-                popup.style.width = imgWidth + "px";
-                popup.style.height = imgHeight + "px";
-                popup.style.top = (screenHeight - imgHeight) / 2 + "px";
-                popup.style.right = "0px";
-
-                popup.style.display = "block";
-            }
-
-        });
-
-        let mouseX = 0;
-        let mouseY = 0;
-
-        // Update mouse coordinates on movement
-        document.addEventListener("mousemove", function(event) {
-            mouseX = event.clientX;
-            mouseY = event.clientY;
-        });
-
-        function checkMousePosition() {
-            if (currentLink) {
-                const rect = currentLink.getBoundingClientRect();
-
-                if (mouseX < rect.left || mouseX > rect.right || mouseY < rect.top || mouseY > rect.bottom) {
-                    // Mouse is outside the coordinates of the link
-                    popup.style.display = "none";
-                    currentLink = null;  // Reset the currently hovered link
-                }
-            }
-
-            // Request the next frame
-            requestAnimationFrame(checkMousePosition);
-        }
-
-        // Start the loop
-        requestAnimationFrame(checkMousePosition);
-    });
 });
 
-*/
+
+window.addEventListener('popstate', function(event) {
+    if (event.state && event.state.hash) {
+        window.location.hash = event.state.hash;
+    }
+});
+
+
+
+
+
